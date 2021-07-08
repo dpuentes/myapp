06b45600da6041adef61ca3d8960bb7b90dfacff
dpuentes <dipuentes123@gmail.com>
2021-07-07 21:13:41 -0500

    Modifies descs for mayusc in models micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(created_at: :DESC) }
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
