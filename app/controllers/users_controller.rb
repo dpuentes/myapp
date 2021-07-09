401706b30b4a704f404c4d7f2d4099096e72d485
dpuentes <dipuentes123@gmail.com>
2021-07-09 12:19:54 -0500

    Creates accion controller logeed user in aplication controller, Creatin accion crete, destroy in micropost controller. Delete accion logged in user  user in controllers. MOdifies tes controller microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,29 +54,21 @@ class UsersController < ApplicationController
 
 	private
 
-	# Confirms an admin user.
-	def admin_user
-		redirect_to(root_url) unless current_user.admin?
-	end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 
-	def user_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation)
-	end
+    # Before filters
 
-	# Before filters
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
-	# confirma un usuario logueado
-	def logged_in_user
-		unless logged_in?
-			store_location
-			flash[:danger] = "Please log in."
-			redirect_to login_url
-		end
-	end
-
-	#confirma el usuario correcto
-	def correct_user
-		@user = User.find(params[:id])
-		redirect_to(root_url) unless current_user?(@user)
-	end
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
