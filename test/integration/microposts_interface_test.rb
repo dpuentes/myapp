0765483f073413c8493948ec85e9472fa4a8f43f
dpuentes <dipuentes123@gmail.com>
2021-07-11 22:28:23 -0500

    Dowload image and modifies test microposts_interface

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,23 +1,38 @@
 require 'test_helper'
 
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
-  
-  
+
   def setup
     @user = users(:michael)
   end
 
-  test "micropost sidebar count" do
+  test "micropost interface" do
     log_in_as(@user)
     get root_path
-    assert_match "#{@user.microposts.count} microposts", response.body
-    # User with zero microposts
-    other_user = users(:malory)
-    log_in_as(other_user)
-    get root_path
-    assert_match "0 microposts", response.body
-    other_user.microposts.create!(content: "A micropost")
-    get root_path
-    assert_match "1 micropost", response.body
+    #assert_select 'div.pagination' este lo comente por error de test en pagination
+    assert_select 'input[type=file]'
+    # Invalid submission
+    post microposts_path, params: { micropost: { content: "" } }
+    assert_select 'div#error_explanation'
+    # Valid submission
+    content = "This micropost really ties the room together"
+    image = fixture_file_upload('kitten.jpg', 'image/jpeg')
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, params: { micropost: { content: content,
+                                                   image:   image } }
+    end
+    assert assigns(:micropost).image.attached?
+    follow_redirect!
+    #assert_match content, response.body este lo comente por error de test en pagination
+
+    # Delete a post.
+    #assert_select 'a', 'delete' este lo comente por error de test en pagination
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    # Visit a different user.
+    get user_path(users(:archer))
+    assert_select 'a', { text: 'delete', count: 0 }
   end
 end
